deea544818d9a347245341a4699b5180ba420988
HemiCarlin <157537106+HemiCarlin@users.noreply.github.com>
2025-11-06 13:53:10 -0600

    cleaned

--- help.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JFrame; 
-import java.util.Random; 
-public class help { 
-    public static void main(String[] args) { 
-        new hate(); 
-    } 
-} 
--- nope.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JFrame; 
-import java.util.Random; 
-public class nope { 
-    public int size = 400; 
-    public nope() { 
-    } 
-} 
